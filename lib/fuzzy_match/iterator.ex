5a5f953b7789ef77d485f9c62d74375ce49ca287
Daniel Pinto Alves <daniel.pinto@booking.com>
2017-02-04 22:21:18 +0100

    Clean trailing spaces

--- lib/fuzzy_match/iterator.ex
+++ lib/fuzzy_match/iterator.ex
@@ -1,11 +1,11 @@
 defmodule FuzzyMatch.Iterator do
   defstruct [ files: %{} ]
-  
+
   def find_match(iterator, pattern, file) do
     points = FuzzyMatch.Pattern.match?(pattern, file)
     add_file(iterator, file, points)
   end
-  
+
   def add_file(iterator, file, points) when points > 20 do
     files = Map.merge(iterator.files, %{ file => points })
     Map.put(iterator, :files, files)
